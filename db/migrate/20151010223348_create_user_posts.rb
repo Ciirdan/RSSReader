972247366134dee38e1770513989b236f24da1aa
Cirdan <172520@supinfo.com>
2015-11-18 11:05:31 +0100

    update db

--- db/migrate/20151010223348_create_user_posts.rb
+++ db/migrate/20151010223348_create_user_posts.rb
@@ -3,8 +3,8 @@ class CreateUserPosts < ActiveRecord::Migration
     create_table :user_posts do |t|
       t.references :user, index: true, foreign_key: true
       t.references :post, index: true, foreign_key: true
-      t.boolean :favorited, :default => 0, :null => false
-      t.boolean :read, :default => 0, :null => false
+      t.boolean :favorited, :default => false, :null => false
+      t.boolean :read, :default => false, :null => false
 
       t.timestamps null: false
     end
